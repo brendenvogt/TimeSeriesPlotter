4eac8851ce01d0b3f614b7789646c38b7511d519
BrendenVogt <bvogtdev@gmail.com>
2018-09-26 19:53:09 -0700

    adjusted initialization and styling

--- main.js
+++ main.js
@@ -28,11 +28,11 @@ function sendCommand() {
 
 function clearScreen() {
     Plotly.deleteTraces('graph', 0)
-    Plotly.addTraces('graph', { y: [0] });
+    Plotly.addTraces('graph', { y: [] });
 }
 
 Plotly.plot('graph', [{
-    y: [0],
+    y: [],
     mode: 'lines',
     line: { color: '#80CAF6' }
 }]);
@@ -46,4 +46,25 @@ var interval = setInterval(function () {
         samples = [];
 
     }
-}, 100);
+}, 1);
+
+
+// Sliders
+
+var slider1 = document.getElementById("slider1");
+var slider2 = document.getElementById("slider2");
+var slider3 = document.getElementById("slider3");
+
+var output = document.getElementById("console");
+
+// output.innerHTML = slider1.value;
+
+slider1.oninput = function () {
+    output.innerHTML = this.value;
+}
+slider2.oninput = function () {
+    output.innerHTML = this.value;
+}
+slider3.oninput = function () {
+    output.innerHTML = this.value;
+}
